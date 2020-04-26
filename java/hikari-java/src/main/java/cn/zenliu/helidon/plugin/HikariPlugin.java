a0bc6d3b2addfbb8dd4f3d4485a9a52d305ea730
ZenLiu <ZenLiu.CN@gmail.com>
2020-04-26 16:27:17 +0800

    update

---
+++ java/hikari-java/src/main/java/cn/zenliu/helidon/plugin/HikariPlugin.java
@@ -0,0 +1,100 @@
+/*
+ *  Copyright (c) 2020.  Zen.Liu .
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *   @Project: helidon-bootstrap
+ *   @Module: hikari-java
+ *   @File: HikariPlugin.java
+ *   @Author:  lcz20@163.com
+ *   @LastModified:  2020-04-26 16:26:45
+ */
+
+package cn.zenliu.helidon.plugin;
+
+import cn.zenliu.helidon.bootstrap.ConfigUtil;
+import cn.zenliu.helidon.bootstrap.Plugin;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import io.helidon.config.Config;
+import io.helidon.webserver.WebServer;
+
+import java.util.Properties;
+import java.util.function.Function;
+
+public interface HikariPlugin extends Plugin {
+    HikariDataSource getDatasource() throws IllegalStateException;
+
+    HikariPlugin configuration(Function<HikariConfig, HikariDataSource> conf);
+
+
+    class HikariPluginImpl implements HikariPlugin {
+        private static final String NAME = "HikariPlugin";
+
+        private HikariPluginImpl() {
+        }
+
+        @Override
+        public String getName() {
+            return NAME;
+        }
+
+        @Override
+        public boolean isBeforeStartServer() {
+            return true;
+        }
+
+        private static Function<HikariConfig, HikariDataSource> configurator = e -> null;
+
+        @Override
+        public HikariDataSource getDatasource() throws IllegalStateException {
+            if (configurator != null) throw new IllegalStateException("HikariPlugin not install");
+            return ds;
+        }
+
+        public HikariPlugin configuration(Function<HikariConfig, HikariDataSource> conf) {
+            configurator = conf;
+            return this;
+        }
+
+        private static HikariDataSource ds;
+
+        @Override
+        public void initialize(Config config, WebServer server) {
+            if (configurator == null) return;
+            Properties prop = ConfigUtil.toProperties(config.get("hikari").detach())
+                    .orElseThrow(() -> new IllegalArgumentException("invalid configuration of hikari"));
+            final HikariConfig conf = new HikariConfig(prop);
+            HikariDataSource ds = configurator.apply(conf);
+            if (ds == null) ds = new HikariDataSource(conf);
+        }
+
+        @Override
+        public void onConfig(Config config) {
+            //do nothing
+        }
+
+        @Override
+        public void doOnFinalize() {
+            if (ds != null) ds.close();
+        }
+
+        private static class HikariPluginHolder {
+            private static final HikariPlugin instance = new HikariPluginImpl();
+        }
+
+    }
+
+    public static HikariPlugin getInstance() {
+        return HikariPluginImpl.HikariPluginHolder.instance;
+    }
+}
--- kotlin/hikari/src/main/kotlin/cn/zenliu/helidon/plugin/HikariPlugin.kt
+++ kotlin/hikari/src/main/kotlin/cn/zenliu/helidon/plugin/HikariPlugin.kt
@@ -1,23 +1,50 @@
+/*
+ *  Copyright (c) 2020.  Zen.Liu .
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *   @Project: helidon-bootstrap
+ *   @Module: hikari
+ *   @File: HikariPlugin.kt
+ *   @Author:  lcz20@163.com
+ *   @LastModified:  2020-04-26 16:27:08
+ */
+
 package cn.zenliu.helidon.plugin
 
 import cn.zenliu.helidon.bootstrap.Plugin
 import cn.zenliu.helidon.bootstrap.toProperties
+import cn.zenliu.helidon.plugin.HikariPlugin.Companion
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.helidon.config.Config
 import io.helidon.webserver.WebServer
 
-class HikariPlugin {
-	companion object : Plugin {
-		const val NAME = "HikariPlugin"
+typealias configurator = Companion.(HikariConfig) -> HikariDataSource?
+
+interface HikariPlugin : Plugin {
+	val datasource: HikariDataSource
+	fun configuration(conf: configurator): HikariPlugin
+
+	companion object : HikariPlugin {
+		private const val NAME = "HikariPlugin"
 		override val name: String = NAME
 		override val beforeStartServer: Boolean = true
 		private lateinit var ds: HikariDataSource
-		val datasource by lazy { ds }
-		private var configurator: (Companion.(HikariConfig) -> HikariDataSource?)? = { null }
-
-		fun configuration(conf: Companion.(HikariConfig) -> HikariDataSource?) {
+		override val datasource: HikariDataSource by lazy { ds }
+		private var configurator: configurator? = { null }
+		override fun configuration(conf: configurator): HikariPlugin {
 			configurator = conf
+			return this
 		}
 
 		override fun initialize(config: Config, server: WebServer) {
@@ -37,7 +64,6 @@ class HikariPlugin {
 		override fun doOnFinalize() {
 			if (configurator == null) ds.close()
 		}
-
 	}
 
 }
